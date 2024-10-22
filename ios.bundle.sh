fc17fb6f50a75db63342e1ab9993a4a42bbe01e7
Michael Amiro <odindo.michael@gmail.com>
2024-10-22 11:21:57 +0300

    Add iOS bundle

---
+++ ios.bundle.sh
@@ -0,0 +1,14 @@
+tap "homebrew/cask"
+tap "homebrew/core"
+
+brew "swiftlint"
+brew "ruby"
+brew "fastlane"
+brew "aria2"
+brew "swiftgen"
+brew "vapor"
+brew "zsh"
+brew "zsh-syntax-highlighting"
+brew "ngrok"
+
+cask "xcodes"
